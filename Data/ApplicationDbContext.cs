099b324d80c7434b1f5e4991562151b9b58e660c
Арина Шпилевая <rina.ress@mail.ru>
2026-07-07 17:00:10 +0500

    Создан контекст базы данных (DbContext)

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CleaningRequests.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleaningRequests.Data
+{
+    /// контекст БД
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CleaningRequest> Requests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
